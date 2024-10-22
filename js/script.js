23a6fc941f7f3a6d33d552b88edf44806e3dbd99
pvdberssel <pim.bersselaar@live.com>
2024-10-22 20:27:30 +0200

    Add function gameboard to addMark in array

--- js/script.js
+++ js/script.js
@@ -3,9 +3,14 @@ var gameboard = {
     board: [],
     init: function(){
         this.board=[['','',''],['','',''],['','','']];
+    },
+    addMark: function(row,column,mark){
+        this.board[row][column] = mark;
     }
     
 }
 console.table(gameboard.board);
 gameboard.init();
+console.table(gameboard.board);
+gameboard.addMark(1,1,'X')
 console.table(gameboard.board);
