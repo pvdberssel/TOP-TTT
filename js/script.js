d1ae1950f5e97067e71d30e7dd14aa22e5de87a9
pvdberssel <pim.bersselaar@live.com>
2024-10-22 20:23:51 +0200

    Add init function to gameboard module

--- js/script.js
+++ js/script.js
@@ -1,15 +1,11 @@
-function gameboard() {
-    let board = [['a1','a2','a3'],['b1','b2','b3'],['c1','c2','c3']]
-    return board
+
+var gameboard = {
+    board: [],
+    init: function(){
+        this.board=[['','',''],['','',''],['','','']];
+    }
     
 }
-
-function displayController(){
-
-}
-
-function player(){
-
-}
-
-console.table(gameboard());
+console.table(gameboard.board);
+gameboard.init();
+console.table(gameboard.board);
