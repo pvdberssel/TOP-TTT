eb779c8486f81b261be7f58b1a62b05bc2922d5d
pvdberssel <pim.bersselaar@live.com>
2024-10-22 20:11:21 +0200

    Add gameboard function and init 3x3 2D array

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.log("Hello");
+function gameboard() {
+    let board = [['a1','a2','a3'],['b1','b2','b3'],['c1','c2','c3']]
+    return board
+    
+}
+
+function displayController(){
+
+}
+
+function player(){
+
+}
+
+console.table(gameboard());
